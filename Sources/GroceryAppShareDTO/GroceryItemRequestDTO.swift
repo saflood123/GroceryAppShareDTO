ab7ac1e7b35e8d7a0334bc479ad992a1fca6266d
saflood123 <stuartflood@gmail.com>
2024-12-29 11:51:29 +0000

    fix

--- Sources/GroceryAppShareDTO/GroceryItemRequestDTO.swift
+++ Sources/GroceryAppShareDTO/GroceryItemRequestDTO.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 public struct GroceryItemRequestDTO: Codable {
-    public var title: String
-    public var quantity: Int
-    public var price: Double
+    public let title: String
+    public let quantity: Int
+    public let price: Double
     
     public init(title: String, quantity: Int, price: Double) {
         self.title = title
