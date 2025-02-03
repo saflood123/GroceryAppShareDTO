55648eea452bf43f549f1a3e7db14066c195591e
saflood123 <stuartflood@gmail.com>
2025-02-03 14:36:20 +0000

    fix item

--- Sources/GroceryAppShareDTO/GroceryItem2RequestDTO.swift
+++ Sources/GroceryAppShareDTO/GroceryItem2RequestDTO.swift
@@ -1,21 +1,24 @@
 //
-//  GroceryResponseDTO.swift
+//  GroceryItemRequestDTO.swift
 //  GroceryAppShareDTO
 //
 //  Created by stuart flood on 29/12/2024.
 //
+
 import Foundation
 
-public struct GroceryItemResponseDTO: Codable {
-    public let id: UUID
+public struct GroceryItem2RequestDTO: Codable {
+   // public let id: UUID
     public let title: String
     public let price: Double
     public let quantity: Int
+
     
-    public init(id: UUID, title: String,price: Double, quantity: Int) {
-        self.id = id
+    public init(title: String, price: Double, quantity: Int) {
+       // self.id = id
         self.title = title
         self.price = price
         self.quantity = quantity
     }
 }
+
