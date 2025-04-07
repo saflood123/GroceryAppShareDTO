43e4d10ba349b2b4855fde4b0c17ded57b82dce3
saflood123 <stuartflood@gmail.com>
2025-04-07 15:00:36 +0300

    fix steps

--- Sources/GroceryAppShareDTO/GroceryItem2ResponseDTO.swift
+++ Sources/GroceryAppShareDTO/GroceryItem2ResponseDTO.swift
@@ -11,18 +11,18 @@ public struct GroceryItem2ResponseDTO: Codable {
     public let title: String
     public let price: Double
     public let quantity: Int
-    public let steps: Int
     public let calories: Int
+    public let steps: Int
     public let dateofbirth: String
     public let date_updated: String
     
-    public init(id: UUID, title: String,price: Double, quantity: Int, steps: Int,calories: Int,dateofbirth: String, date_updated: String) {
+    public init(id: UUID, title: String,price: Double, quantity: Int, calories: Int,steps: Int, dateofbirth: String, date_updated: String) {
         self.id = id
         self.title = title
         self.price = price
         self.quantity = quantity
-        self.steps = steps
         self.calories = calories
+        self.steps = steps
         self.dateofbirth = dateofbirth
         self.date_updated = date_updated
     }
