7622383017f9f64d4ce33ec57863c33eb6e58ad7
saflood123 <stuartflood@gmail.com>
2025-04-19 17:33:50 +0100

    reduce cols

--- Sources/GroceryAppShareDTO/GroceryItem3RequestDTO.swift
+++ Sources/GroceryAppShareDTO/GroceryItem3RequestDTO.swift
@@ -12,21 +12,22 @@ public struct GroceryItem3RequestDTO: Codable {
     public let title: String
     public let price: Double
     public let quantity: Int
-    public let calories: Int
-    public let steps: Int
-    public let dateofbirth: String
-    public let date_updated: String
+//    public let calories: Int
+//    public let steps: Int
+//    public let dateofbirth: String
+//    public let date_updated: String
 
     
-    public init(title: String, price: Double, quantity: Int, calories: Int,steps: Int, dateofbirth: String, date_updated: String) {
+    public init(title: String, price: Double, quantity: Int) {
+        //, calories: Int,steps: Int, dateofbirth: String, date_updated: String
        // self.id = id
         self.title = title
         self.price = price
         self.quantity = quantity
-        self.calories = calories
-        self.steps = steps
-        self.dateofbirth = dateofbirth
-        self.date_updated = date_updated
+//        self.calories = calories
+//        self.steps = steps
+//        self.dateofbirth = dateofbirth
+//        self.date_updated = date_updated
     }
 }
 
